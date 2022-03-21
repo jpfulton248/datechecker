a23fd292cff6f07ea7f001cac50b8db5f2d28b9a
Peter Fulton <jpfulton@Peters-MacBook-Pro.local>
2022-03-20 22:05:41 -0400

    Working but needs a lot of formatting

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -5,10 +5,13 @@ import datetime
 import pandas as pd
 from markupsafe import Markup
 import re
-
 app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemyurl
 db = SQLAlchemy(app)
+from collections import OrderedDict, defaultdict
+import json
+from itertools import groupby
+import calendar
 
 class Main(db.Model):
     mainid = db.Column(db.Integer(), primary_key=True)
@@ -21,6 +24,7 @@ class Main(db.Model):
 class earningsdates(db.Model):
     earningsdateid = db.Column(db.Integer(), primary_key=True)
     ticker = db.Column(db.String(15))
+    companyname = db.Column(db.String(100))
     exactearningsdate = db.Column(db.String())
 
 @app.route('/<string:ticker>')
@@ -32,51 +36,47 @@ def hello(ticker):
         market_cap = theticker.market_cap
         avg_stockvol = theticker.avg_stockvol
         theticker = theticker.ticker
+        market_cap = round((market_cap/1000000000), 2)
     except:
         pass
     edate = earningsdates.query.filter_by(ticker = ticker).first()
     exactearningsdate = edate.exactearningsdate
+    edatestr = datetime.datetime.strftime(exactearningsdate,'%-m/%-d/%Y %-H')
     thisticker = edate.ticker
+    edatestr, bmoamc = edatestr.split()
+    bmoamc = bmoamc.replace('8', 'Before Market Open')
+    bmoamc = bmoamc.replace('16', 'After Market Close')
     now = datetime.datetime.now()
-    #condition = earningsdates.exactearningsdate > now
-    sidebar = earningsdates.query \
-        .with_entities(earningsdates.ticker, earningsdates.exactearningsdate) \
+    result = earningsdates.query \
+        .with_entities(earningsdates.ticker, earningsdates.exactearningsdate, earningsdates.companyname) \
         .filter(earningsdates.exactearningsdate > now).order_by(earningsdates.ticker).all()
-    df = pd.DataFrame (sidebar, columns =['ticker', 'exactearningsdate'])
+    
+    df = pd.DataFrame(result, columns =['ticker', 'exactearningsdate', 'companyname'])
     df['exactearningsdatestr'] = df['exactearningsdate'].dt.strftime('%-m/%-d/%-y %-H')
+    df['earningsdow'] = df['exactearningsdate'].dt.day_name()
     df[['exactearningsdatestr', 'time']] = df['exactearningsdatestr'].str.split(' ', n=1, expand=True)
-    df = df.drop(columns=['exactearningsdate'])
-    df['time'] = df['time'].replace('8', 'Before Market Open')
-    df['time'] = df['time'].replace('16', 'After Market Close')
+    df['time'] = df['time'].replace('8', 'BMO')
+    df['time'] = df['time'].replace('16', 'AMC')
     df = df.sort_values(['exactearningsdatestr', 'time'], ascending=[True, False])
-    df['ticker'] = '<a href="' + df.ticker.map(str) + '">'  + df.ticker.map(str) + '</a>'
-    df.set_index(['exactearningsdatestr', 'time', 'ticker'], inplace=True)
-    df.groupby('exactearningsdatestr').indices
-    table = df.to_html(escape=False, index=True, header=False, render_links=True).replace('border="1"', '')
-    table = re.sub(r'(?:<th rowspan=\"\d+\" valign=\"top\">)', '<div class=\"edate\">', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:</th>)', '</div>', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:<th>)', '<div class=\"aticker\">', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:\"edate\">Before)', '\"bmoamc\">Before', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:\"edate\">After)', '\"bmoamc\">After', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:\s</tr>)', '', table, 0, re.MULTILINE)
-    table = re.sub(r'(?:<tr>)', '', table, 0, re.MULTILINE)
+    df = df.drop(columns='exactearningsdate')
+    mydict = df.to_dict(orient='rows')
 
-    # table = table.replace('exactearningsdatestr', '').replace('time','').replace('ticker','')
-    # table = table.replace('      ', '<br/>')
+    lists = {}
 
+    for k, g in groupby(mydict, key=lambda t: t['earningsdow']):
+        lists[k] = list(g)
 
     try:
         return render_template('index.html', 
             theticker = thisticker,
             companyname = companyname,
-            avg_optvol = avg_optvol,
+            avg_optvol = f'{int(avg_optvol):,}',
             market_cap = market_cap,
-            avg_stockvol = avg_stockvol,
-            earningsdate = exactearningsdate,
-            thedate = now,
-            tables = table)
+            avg_stockvol = f'{int(avg_stockvol):,}',
+            earningsdate = edatestr,
+            bmoamc = bmoamc,
+            lists = lists)
     except:
         return render_template('index.html', 
             companyname = "This doesn't exist",
-            tables = table)
-        
+            lists=lists)        
