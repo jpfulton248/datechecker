d1f2752dbb1f9a2e864749230df30158c0b7ec06
Peter Fulton <jpfulton@Peters-MacBook-Pro.local>
2022-03-31 17:55:57 -0400

    still still launching

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from flaskr.mm import app
-from dotenv import load_dotenv
-import os
-load_dotenv
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+# from dotenv import load_dotenv
+# import os
+# load_dotenv
+# SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
  
 
 if __name__ == "__mm__":
