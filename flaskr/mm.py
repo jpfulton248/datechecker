b55e6cb8f667e079df8d5e1ed23cd42edec47575
Peter Fulton <jpfulton@Peters-MacBook-Pro.local>
2022-03-30 20:09:18 -0400

    still launching

--- flaskr/mm.py
+++ flaskr/mm.py
@@ -11,7 +11,7 @@ import re
 from dotenv import load_dotenv
 import os
 load_dotenv
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
